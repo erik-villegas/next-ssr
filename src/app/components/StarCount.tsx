558a2ca4bee04aecaf447a45d762d5fb673a742b
Erik Villegas <erik.villegas@catperson.com>
2023-10-18 13:55:04 -0700

    updates

--- src/app/components/StarCount.tsx
+++ src/app/components/StarCount.tsx
@@ -1,5 +1,5 @@
 export default async function StarCounter(props: {repo: string, delay: number}) {
-  await new Promise(r => setTimeout(r, props.delay));
+  await new Promise(r => setTimeout(r, 2000));
   const res = await fetch(`https://api.github.com/repos/${props.repo}`)
   const repo = await res.json()
   const stars = repo.stargazers_count

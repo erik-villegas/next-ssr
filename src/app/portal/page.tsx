4442ecbf80c3b4567d28ef773689cfe688972005
Erik Villegas <erik.villegas@catperson.com>
2023-10-18 14:02:43 -0700

    updates

--- src/app/portal/page.tsx
+++ src/app/portal/page.tsx
@@ -1,29 +1,24 @@
 import { Suspense } from "react";
 import StarCounter from "../components/StarCount";
 
-
 function Loading() {
-  return <h1>Server component loading!</h1>
+  return <h1>Server component loading!</h1>;
 }
 
 export default async function Portal() {
-  
   return (
     <main>
       <Suspense fallback={<p>Loading stars...</p>}>
-        <StarCounter repo={'facebook/react'} delay={2000}/>
+        <StarCounter repo={"facebook/react"} delay={2000} />
       </Suspense>
 
       <Suspense fallback={<p>Loading stars...</p>}>
-      <StarCounter repo={'vercel/next.js'} delay={3000}/>
+        <StarCounter repo={"vercel/next.js"} delay={3000} />
       </Suspense>
 
       <Suspense fallback={<p>Loading stars...</p>}>
-        <StarCounter repo={'tailwindlabs/tailwindcss'} delay={4000}/>
+        <StarCounter repo={"tailwindlabs/tailwindcss"} delay={4000} />
       </Suspense>
-
-        
-        
     </main>
-  )
+  );
 }
